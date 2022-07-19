99b8e366c65a5217c8aadd0760cedf3e9ae083ad
Jared Flack <jared.flack@gmail.com>
2022-07-19 10:30:35 -0400

    Update artisan.plugin.zsh

--- artisan.plugin.zsh
+++ artisan.plugin.zsh
@@ -13,44 +13,14 @@ function artisan() {
         >&2 echo "zsh-artisan: artisan not found. Are you in a Laravel directory?"
         return 1
     fi
-
-    local laravel_path=`dirname $artisan_path`
-    local docker_compose_config_path=`find $laravel_path -maxdepth 1 \( -name "docker-compose.yml" -o -name "docker-compose.yaml" \) | head -n1`
-    local artisan_cmd
-
-    if [ "$docker_compose_config_path" = '' ]; then
-        artisan_cmd="php $artisan_path"
-    else
-        if [ "`grep "laravel/sail" $docker_compose_config_path | head -n1`" != '' ]; then
-            artisan_cmd="$laravel_path/vendor/bin/sail artisan"
-        else
-            local docker_compose_cmd=`_docker_compose_cmd`
-            local docker_compose_service_name=`$docker_compose_cmd ps --services 2>/dev/null | grep 'app\|php\|api\|workspace\|laravel\.test\|webhost' | head -n1`
-            if [ -t 1 ]; then
-                artisan_cmd="$docker_compose_cmd exec $docker_compose_service_name php artisan"
-            else
-                # The command is not being run in a TTY (e.g. it's being called by the completion handler below)
-                artisan_cmd="$docker_compose_cmd exec -T $docker_compose_service_name php artisan"
-            fi
-        fi
-    fi
+    
+    local artisan_cmd="docker-compose exec app php artisan"
 
     local artisan_start_time=`date +%s`
 
     eval $artisan_cmd $*
 
     local artisan_exit_status=$? # Store the exit status so we can return it later
-
-    if [[ $1 = "make:"* && $ARTISAN_OPEN_ON_MAKE_EDITOR != "" ]]; then
-        # Find and open files created by artisan
-        find \
-            "$laravel_path/app" \
-            "$laravel_path/tests" \
-            "$laravel_path/database" \
-            -type f \
-            -newermt "-$((`date +%s` - $artisan_start_time + 1)) seconds" \
-            -exec $ARTISAN_OPEN_ON_MAKE_EDITOR {} \; 2>/dev/null
-    fi
 
     return $artisan_exit_status
 }
